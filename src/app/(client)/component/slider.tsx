0184315f8b9acbd37abca97dcf6e129ec476e7bc
caotruongwf1204 <caodinhtruong1204@gmail.com>
2023-12-14 22:38:38 +0700

    up

---
+++ src/app/(client)/component/slider.tsx
@@ -0,0 +1,29 @@
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "@/app/css/slider.css";
+import { Pagination, Navigation } from "swiper/modules";
+
+export default function SimpleSlider() {
+  return (
+    <>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
